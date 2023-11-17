df15c470d8da96acbcac3323bcb3deb5b442a9c2
Ishan Mukherjee <ishanjmukherjee@gmail.com>
2023-11-17 10:44:35 +0530

    Update find_bases.m
    added test case outputs

--- hw-program-8/find_bases.m
+++ hw-program-8/find_bases.m
@@ -88,3 +88,338 @@ cs
 ns
 coords_cs
 coords_ns
+
+% 1. A = [1 2 -4 -3 0;-2 -3 5 8 8;2 2 -2 -9 -13]; b = [1;8;2];
+% The column space is a 3 dimensional subspace of R^3
+% The null space is a 2 dimensional subspace of R^5
+% 
+% b is in R^3 and is in the column space of A, and its coordinates are:
+% 121.000000
+% -30.000000
+% 20.000000
+% 
+% cs =
+% 
+%      1     2    -3
+%     -2    -3     8
+%      2     2    -9
+% 
+% 
+% ns =
+% 
+%     -2    -5
+%      3    -2
+%      1     0
+%      0    -3
+%      0     1
+% 
+% 
+% coords_cs =
+% 
+%    121
+%    -30
+%     20
+% 
+% 
+% coords_ns =
+% 
+%      []
+
+% 2. A = [1 2 -4 -3 0;-2 -3 5 8 8;2 2 -2 -9 -13]; b = [1;8;2;3];
+% The column space is a 3 dimensional subspace of R^3
+% The null space is a 2 dimensional subspace of R^5
+% 
+% b is R^4 and is in neither nul A nor col A
+% 
+% cs =
+% 
+%      1     2    -3
+%     -2    -3     8
+%      2     2    -9
+% 
+% 
+% ns =
+% 
+%     -2    -5
+%      3    -2
+%      1     0
+%      0    -3
+%      0     1
+% 
+% 
+% coords_cs =
+% 
+%      []
+% 
+% 
+% coords_ns =
+% 
+%      []
+
+% 3. A = [1 2 -4 -3 0;-2 -3 5 8 8;2 2 -2 -9 -13]; b = [1;8;2;3;-1];
+% The column space is a 3 dimensional subspace of R^3
+% The null space is a 2 dimensional subspace of R^5
+% 
+% b is in R^5 and is in the null space of A, and its coordinates are:
+% 2.000000
+% -1.000000
+% 
+% cs =
+% 
+%      1     2    -3
+%     -2    -3     8
+%      2     2    -9
+% 
+% 
+% ns =
+% 
+%     -2    -5
+%      3    -2
+%      1     0
+%      0    -3
+%      0     1
+% 
+% 
+% coords_cs =
+% 
+%      []
+% 
+% 
+% coords_ns =
+% 
+%     2.0000
+%    -1.0000
+
+% 4. A = [1 2 -4 -3 0;-2 -3 5 8 8;2 2 -2 -9 -13]; b = [1;8;2;3;-2];
+% The column space is a 3 dimensional subspace of R^3
+% The null space is a 2 dimensional subspace of R^5
+% 
+% b is in R^5 but is not in the null space of A
+% 
+% cs =
+% 
+%      1     2    -3
+%     -2    -3     8
+%      2     2    -9
+% 
+% 
+% ns =
+% 
+%     -2    -5
+%      3    -2
+%      1     0
+%      0    -3
+%      0     1
+% 
+% 
+% coords_cs =
+% 
+%      []
+% 
+% 
+% coords_ns =
+% 
+%      []
+
+% 5. A = [1 0 2;0 1 3;0 0 0]; b = [1;2;3];
+% The column space is a 2 dimensional subspace of R^3
+% The null space is a 1 dimensional subspace of R^3
+% 
+% b is in R^3 but is not in the column space of A
+% b is in R^3 but is not in the null space of A
+% 
+% cs =
+% 
+%      1     0
+%      0     1
+%      0     0
+% 
+% 
+% ns =
+% 
+%     -2
+%     -3
+%      1
+% 
+% 
+% coords_cs =
+% 
+%      []
+% 
+% 
+% coords_ns =
+% 
+%      []
+
+% 6. A = [2 -2;2 -2];b = [1;1];
+% The column space is a 1 dimensional subspace of R^2
+% The null space is a 1 dimensional subspace of R^2
+% 
+% b is in R^2 and is in the column space of A, and its coordinates are:
+% 0.500000
+% b is in R^2 and is in the null space of A, and its coordinates are:
+% 1.000000
+% 
+% cs =
+% 
+%      2
+%      2
+% 
+% 
+% ns =
+% 
+%      1
+%      1
+% 
+% 
+% coords_cs =
+% 
+%     0.5000
+% 
+% 
+% coords_ns =
+% 
+%     1.0000
+
+% 7. A = zeros(3);b = zeros(3,1);
+% The column space is a 0 dimensional subspace of R^3
+% The null space is a 3 dimensional subspace of R^3
+% 
+% b is in R^3 but is not in the column space of A
+% b is in R^3 and is in the null space of A, and its coordinates are:
+% 0.000000
+% 0.000000
+% 0.000000
+% 
+% cs =
+% 
+%   3×0 empty double matrix
+% 
+% 
+% ns =
+% 
+%      1     0     0
+%      0     1     0
+%      0     0     1
+% 
+% 
+% coords_cs =
+% 
+%      []
+% 
+% 
+% coords_ns =
+% 
+%      0
+%      0
+%      0
+
+% 8. A = eye(3);b = zeros(3,1);
+% The column space is a 3 dimensional subspace of R^3
+% The null space is a 0 dimensional subspace of R^3
+% 
+% b is in R^3 and is in the column space of A, and its coordinates are:
+% 0.000000
+% 0.000000
+% 0.000000
+% b is in R^3 and is in the null space of A, and its coordinates are:
+% 
+% 
+% cs =
+% 
+%      1     0     0
+%      0     1     0
+%      0     0     1
+% 
+% 
+% ns =
+% 
+%   3×0 empty double matrix
+% 
+% 
+% coords_cs =
+% 
+%      0
+%      0
+%      0
+% 
+% 
+% coords_ns =
+% 
+%   0×1 empty double column vector
+
+% 9. A = [1 0 0 1;0 1 0 2;0 0 1 3;0 0 -1/3 -1]; b = [1;2;3;-1];
+% The column space is a 3 dimensional subspace of R^4
+% The null space is a 1 dimensional subspace of R^4
+% 
+% b is in R^4 and is in the column space of A, and its coordinates are:
+% 1.000000
+% 2.000000
+% 3.000000
+% b is in R^4 and is in the null space of A, and its coordinates are:
+% -1.000000
+% 
+% cs =
+% 
+%     1.0000         0         0
+%          0    1.0000         0
+%          0         0    1.0000
+%          0         0   -0.3333
+% 
+% 
+% ns =
+% 
+%     -1
+%     -2
+%     -3
+%      1
+% 
+% 
+% coords_cs =
+% 
+%     1.0000
+%     2.0000
+%     3.0000
+% 
+% 
+% coords_ns =
+% 
+%    -1.0000
+
+% 10. A = [1 0 0 1;0 1 0 2;0 0 1 3;0 0 -1/3 -1];
+% The column space is a 3 dimensional subspace of R^4
+% The null space is a 1 dimensional subspace of R^4
+% 
+% b is in R^4 and is in the column space of A, and its coordinates are:
+% 1.000000
+% 2.000000
+% 3.000000
+% b is in R^4 and is in the null space of A, and its coordinates are:
+% -1.000000
+% 
+% cs =
+% 
+%     1.0000         0         0
+%          0    1.0000         0
+%          0         0    1.0000
+%          0         0   -0.3333
+% 
+% 
+% ns =
+% 
+%     -1
+%     -2
+%     -3
+%      1
+% 
+% 
+% coords_cs =
+% 
+%     1.0000
+%     2.0000
+%     3.0000
+% 
+% 
+% coords_ns =
+% 
+%    -1.0000
